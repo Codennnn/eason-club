ac96e44ee59af447b4dc596ca635210d49125a8c
令狐少侠 <czc12580520@gmail.com>
2020-07-02 00:04:16 +0800

    commit

--- src/pages/club/index.js
+++ src/pages/club/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
+import ScrollableTabView, {
+  ScrollableTabBar,
+} from 'react-native-scrollable-tab-view'
 
 import ClubHeader from './components/ClubHeader'
 
@@ -18,6 +21,19 @@ export default () => {
         name={info.owner.name}
         introduction={info.introduction}
       />
+
+      <View>
+        <ScrollableTabView
+          style={{ marginTop: 20 }}
+          initialPage={2}
+          renderTabBar={() => <ScrollableTabBar />}>
+          <Text tabLabel="Tab #1">My</Text>
+          <Text tabLabel="Tab #2 word word">favorite</Text>
+          <Text tabLabel="Tab #3 word word word">project</Text>
+          <Text tabLabel="Tab #4 word word word word">favorite</Text>
+          <Text tabLabel="Tab #5">project</Text>
+        </ScrollableTabView>
+      </View>
     </SafeAreaView>
   )
 }
