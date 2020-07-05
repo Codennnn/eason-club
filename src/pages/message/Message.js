3f09b0c1193e0a5d0e6172e29d5befcddbcd0cdb
令狐少侠 <czc12580520@gmail.com>
2020-07-05 23:23:34 +0800

    commit

--- src/pages/message/Message.js
+++ src/pages/message/Message.js
@@ -1,15 +1,5 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-import ScrollableTabView from 'react-native-scrollable-tab-view'
-import { primary, secondary } from '@/config/style.config'
-import CustomTabBar from '@comp/CustomTabBar'
-
-const css = StyleSheet.create({
-  tabBarUnderline: {
-    backgroundColor: primary,
-    borderRadius: 5,
-  },
-})
 
 export default () => {
   return (
@@ -20,10 +10,8 @@ export default () => {
       tabBarUnderlineStyle={css.tabBarUnderline}
       scrollWithoutAnimation={true}
       renderTabBar={() => <CustomTabBar />}>
-      <Text tabLabel="主页">My</Text>
       <Text tabLabel="动态">favorite</Text>
-      <Text tabLabel="成员">project</Text>
-      <Text tabLabel="招新">favorite</Text>
+      <Text tabLabel="消息">My</Text>
     </ScrollableTabView>
   )
 }
--- src/pages/message/index.js
+++ src/pages/message/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+import ScrollableTabView from 'react-native-scrollable-tab-view'
+import { primary, secondary } from '@/config/style.config'
 
-export default function() {
+import CustomTabBar from '@comp/CustomTabBar'
+
+const css = StyleSheet.create({
+  tabBarUnderline: {
+    backgroundColor: primary,
+    borderRadius: 5,
+  },
+})
+
+export default () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollableTabView
+      style={{ backgroundColor: 'white' }}
+      tabBarActiveTextColor={primary}
+      tabBarInactiveTextColor={secondary}
+      tabBarUnderlineStyle={css.tabBarUnderline}
+      scrollWithoutAnimation={true}
+      renderTabBar={() => <CustomTabBar />}>
+      <Text tabLabel="动态">favorite</Text>
+      <Text tabLabel="消息">My</Text>
+    </ScrollableTabView>
   )
 }
